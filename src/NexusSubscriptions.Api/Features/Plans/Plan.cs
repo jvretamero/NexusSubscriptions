17f5b027750e534ce82cd21ceb994d1e6df262ba
João Vitor Retamero <joaovretamero@gmail.com>
2026-03-16 14:19:06 -0300

    Plan entity definitions

--- src/NexusSubscriptions.Api/Features/Plans/Plan.cs
+++ src/NexusSubscriptions.Api/Features/Plans/Plan.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace NexusSubscriptions.Api.Features.Plans;
 
 public class Plan
@@ -7,4 +10,20 @@ public class Plan
     public required string Description { get; set; }
 
     public decimal Price { get; set; }
+}
+
+public class PlanTypeConfiguration : IEntityTypeConfiguration<Plan>
+{
+    public void Configure(EntityTypeBuilder<Plan> builder)
+    {
+        builder.ToTable("Plans")
+            .HasKey(p => p.Id);
+
+        builder.Property(p => p.Description)
+            .HasMaxLength(30)
+            .IsRequired();
+
+        builder.Property(p => p.Price)
+            .IsRequired();
+    }
 }
--- src/NexusSubscriptions.Api/Infrasctructure/Database/ApiContext.cs
+++ src/NexusSubscriptions.Api/Infrasctructure/Database/ApiContext.cs
@@ -6,4 +6,11 @@ namespace NexusSubscriptions.Api.Infrasctructure.Database;
 public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
 {
     public DbSet<Plan> Plans { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApiContext).Assembly);
+    }
 }
